a4d52edf4ac4fc4a98e673884744548f515ea0e0
Sean Keane <seankeane@live.ie>
2021-09-13 10:52:27 +0100

    created standalone functions - pre cleanup

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const { App } = require("@slack/bolt");
-const util = require('util');
-const axios = require('axios');
+
+const util = require("util");
+const axios = require("axios");
 
 require("dotenv").config();
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.event('app_home_opened', async ({ event, client }) => {
+  try {
+    // Call views.publish with the built-in client
+    const result = await client.views.publish({
+      // Use the user ID associated with the event
+      user_id: event.user,
+      view: homeView
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.log("Error during app_home_opened event handling");
+    console.error(error);
+  }
 });
 
 app.action('movie_button', async ({ ack, body, client }) => {
@@ -19,45 +36,34 @@ app.action('movie_button', async ({ ack, body, client }) => {
       view: modalView
     });
   } catch (error) {
-    console.log("err")
+    console.log("Error during movie_button action handling");
     console.error(error);
   }
 });
 
-app.action('movie_select', async ({ ack, body, client }) => {
+app.options('movie_select', async ({ options, ack }) => {
   try {
-    await ack();
+    let searchString = options.value.toLowerCase();
 
-    // Call the views.open method using the WebClient passed to listeners
-    const result = await client.views.open({
-      trigger_id: body.trigger_id,
-      view: modalView
+    // filter the saved data set based on the input search string and return array with only matching records
+    const opt = presetMovieData.filter(movieRes => movieRes.text.text.toLowerCase().includes(searchString));
+    console.log("Typeahead Search String" + searchString);
+    console.log("Filter Results:" + util.inspect(opt, {depth: null}));
+
+    await ack({
+      "options": opt
     });
-
-    console.log(result);
   } catch (error) {
-    console.log("err")
+    console.log("Error during movie_select options handling");
     console.error(error);
   }
-});
-
-app.options({'action_id': 'movie_select'}, async ({ options, ack }) => {
-  let searchString = options.value.toLowerCase();
-
-  const opt = presetMovieData.filter(x => x.text.text.toLowerCase().includes(searchString));
-  //console.log("searchString:" + searchString);
-  //console.log("opt:" + util.inspect(opt, {depth: null}));
-
-  await ack({
-    "options": opt
-  });
 });
 
 app.action('movie_select', ({ ack }) => {
   ack();
 });
 
-app.view('movie_modal_submit', async ({ ack, body, payload, client }) => {
+app.view('movie_modal_submit', async ({ logger, ack, body, payload, client }) => {
   try {
     await ack();
 
@@ -65,21 +71,33 @@ app.view('movie_modal_submit', async ({ ack, body, payload, client }) => {
 
     let url = `https://api.themoviedb.org/3/movie/${selectedMovieId}?api_key=${process.env.MOVIE_DB_AUTH_KEY}`;
 
-    let selectedMovieTitle = "Movie Title";
+    // TODO Remove dummy set values - move to success handler function of axios request and make that a standalone function
+    /*let selectedMovieTitle = "Movie Title";
     let selectedMovieReleaseDate = "Movie Release Date";
     let selectedDescription = "Movie Description";
     let selectedMoviePosterUrl = "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg";
+    */
 
 
     // GET request with Axios to TMDB to get movie details
-    axios.get(url)
-    .then(function (response) {
-      if (response) {
-        //console.log("responsedata:" + util.inspect(response, {depth: null}));
-        selectedMovieTitle = response.data.title;
-        selectedDescription = response.data.overview;
-        selectedMovieReleaseDate = response.data.release_date;
-        selectedMoviePosterUrl = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${response.data.poster_path}`;
+    // TODO have success handler a function declaration
+    //const response = await axios.get(url);
+    const response = await requestMovieDetailsFromTMDB(selectedMovieId);
+
+    //axios.get(url)
+    //.then(function (response) {
+      if (response && response.data) {
+
+        const messageData = constructDMFromResponse(response.data);
+
+
+        /*
+        // update variables with data from API request
+        const selectedMovieTitle = response.data.title;
+        const selectedDescription = response.data.overview;
+        const selectedMovieReleaseDateString = response.data.release_date;
+        const selectedMovieReleaseDateUnix = new Date(selectedMovieReleaseDateString).getTime() / 1000;
+        const selectedMoviePosterUrl = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${response.data.poster_path}`;
 
         const messageBlock = [
           {
@@ -101,12 +119,12 @@ app.view('movie_modal_submit', async ({ ack, body, payload, client }) => {
             "type": "section",
             "text": {
               "type": "mrkdwn",
-              "text": `*Release date*: ${selectedMovieReleaseDate}\n${selectedDescription}.`
+              "text": `*Release date*: <!date^${selectedMovieReleaseDateUnix}^{date_short}|${selectedMovieReleaseDateString}>\n${selectedDescription}.`
             },
             "accessory": {
               "type": "image",
               "image_url": selectedMoviePosterUrl,
-              "alt_text": "cute cat"
+              "alt_text": "Movie Poster"
             }
           },
           {
@@ -114,39 +132,104 @@ app.view('movie_modal_submit', async ({ ack, body, payload, client }) => {
           }
         ];
 
+        const backupMessageText = `Movie Title: ${selectedMovieTitle}, Release Date: ${selectedMovieReleaseDateString}, Description: ${selectedDescription}`;
+        */
+
+
+        const dmResult = await sendDMToUser(client, body.user.id, messageData.messageBlock, messageData.backupMessageText);
+        /*
+
         const result = client.chat.postMessage({
           channel: body.user.id,
           blocks: messageBlock,
-          text: `Movie Title: ${selectedMovieTitle}, Release Date: ${selectedMovieReleaseDate}, Description: ${selectedDescription}`
+          text: backupMessageText
         });
+        */
+
+
       }
-      
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 
   } catch (error) {
-    console.log("err")
+    console.log("Error during movie_modal_submit view handling");
     console.error(error);
   }
 });
 
-app.event('app_home_opened', async ({ event, client }) => {
+
+
+function constructDMFromResponse(data) {
+  const responseData = data;
+  const selectedMovieTitle = data.title;
+  const selectedDescription = data.overview;
+  const selectedMovieReleaseDateString = data.release_date;
+  const selectedMovieReleaseDateUnix = new Date(selectedMovieReleaseDateString).getTime() / 1000;
+  const selectedMoviePosterUrl = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+
+  const backupMessageText = `Movie Title: ${selectedMovieTitle}, Release Date: ${selectedMovieReleaseDateString}, Description: ${selectedDescription}`;
+
+
+  const messageBlock = [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "Here's the movie info you requested!"
+      }
+    },
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": selectedMovieTitle,
+        "emoji": true
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `*Release date*: <!date^${selectedMovieReleaseDateUnix}^{date_short}|${selectedMovieReleaseDateString}>\n${selectedDescription}.`
+      },
+      "accessory": {
+        "type": "image",
+        "image_url": selectedMoviePosterUrl,
+        "alt_text": "Movie Poster"
+      }
+    },
+    {
+      "type": "divider"
+    }
+  ];
+
+  return {messageBlock, backupMessageText};
+
+}
+
+function requestMovieDetailsFromTMDB(movieId) {
   try {
-    // Call views.publish with the built-in client
-    const result = await client.views.publish({
-      // Use the user ID associated with the event
-      user_id: event.user,
-      view: homeView
-    });
-
-    console.log(result);
-  }
-  catch (error) {
+    const reqUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.MOVIE_DB_AUTH_KEY}`;
+    return axios.get(reqUrl);
+  } catch (error) {
+    console.log("Error response from TMDB request");
     console.error(error);
   }
-});
+}
+
+async function sendDMToUser(client, channel, block, text) {
+  const dmResult = await client.chat.postMessage({
+    channel: channel,
+    blocks: block,
+    text: text
+  });
+
+  if (dmResult.ok) {
+    console.log("DM sent successfully");
+    console.log("DM result body:" + util.inspect(dmResult, {depth: null}));
+  } else {
+    console.log("DM failed to send");
+    console.log("DM result body:" + util.inspect(dmResult, {depth: null}));
+  }
+}
 
 const homeView = {
   // Home tabs must be enabled in your app configuration page under "App Home"
